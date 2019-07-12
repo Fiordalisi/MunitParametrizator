0ab5a7032ccea7843ab9a33e05fa90716e04f505
Fiordalisi <te.fiordalisi@gmail.com>
2019-07-12 16:00:40 -0300

    Added json schemas

--- src/main/java/Workday.java
+++ src/main/java/Workday.java
@@ -34,36 +34,36 @@ public class Workday {
     }
 
     public static void generateExclude(){
-        String asd[] = {"AddAcademicAppointmentTestCase.xml\n" ,
-                "CancelSupplierInvoiceTestCase.xml\n",
-                "CloseJobRequisitionTestCase.xml\n",
-                "EnterTimeOffTestCase.xml\n",
-                "GetCompensationPlansTestCase.xml\n",
-                "GetCustomListsTestCase.xml\n",
-                "GetDegreesTestCase.xml\n",
-                "GetEmployeeAwardsTestCase.xml\n",
-                "GetHeadcountPlanStructuresTestCase.xml\n",
-                "GetPaymentTypesTestCase.xml\n",
-                "GetPayrollCANYearEndsFormsTestCase.xml\n",
-                "GetPayrollPayeeNIsTestCase.xml\n",
-                "GetPayrollPayeePT1sTestCase.xml\n",
-                "GetPeriodSchedulesTestCase.xml\n",
-                "GetTextBlockCategoryTestCase.xml\n",
-                "GetWorkdayAccountSignOnsTestCase.xml\n",
-                "performance-management-flows.xml\n",
-                "performance-management-tests.xml\n",
-                "PutBenefitAnnualRateTestCase.xml\n",
-                "PutCompetencyTestCase.xml\n",
-                "PutFundTestCase.xml\n",
-                "PutIntegrationMessageTestCase.xml\n",
-                "PutStudentApplicationTestCase.xml\n",
-                "PutStudentCompetencyTestCase.xml\n",
-                "PutStudentEducationalInstitutionTestCase.xml\n",
-                "PutTimeClockEventsTestCase.xml\n",
-                "PutTranslatableTenantDataPublicTestCase.xml\n",
-                "SubmitDonorContributionIT.xml\n",
-                "SubmitDonorContributionTestCase.xml\n",
-                "SubmitStudentRecruiterTestCase.xml\n",
+        String asd[] = {"AddAcademicAppointmentTestCase.xml" ,
+                "CancelSupplierInvoiceTestCase.xml",
+                "CloseJobRequisitionTestCase.xml",
+                "EnterTimeOffTestCase.xml",
+                "GetCompensationPlansTestCase.xml",
+                "GetCustomListsTestCase.xml",
+                "GetDegreesTestCase.xml",
+                "GetEmployeeAwardsTestCase.xml",
+                "GetHeadcountPlanStructuresTestCase.xml",
+                "GetPaymentTypesTestCase.xml",
+                "GetPayrollCANYearEndsFormsTestCase.xml",
+                "GetPayrollPayeeNIsTestCase.xml",
+                "GetPayrollPayeePT1sTestCase.xml",
+                "GetPeriodSchedulesTestCase.xml",
+                "GetTextBlockCategoryTestCase.xml",
+                "GetWorkdayAccountSignOnsTestCase.xml",
+                "performance-management-flows.xml",
+                "performance-management-tests.xml",
+                "PutBenefitAnnualRateTestCase.xml",
+                "PutCompetencyTestCase.xml",
+                "PutFundTestCase.xml",
+                "PutIntegrationMessageTestCase.xml",
+                "PutStudentApplicationTestCase.xml",
+                "PutStudentCompetencyTestCase.xml",
+                "PutStudentEducationalInstitutionTestCase.xml",
+                "PutTimeClockEventsTestCase.xml",
+                "PutTranslatableTenantDataPublicTestCase.xml",
+                "SubmitDonorContributionIT.xml",
+                "SubmitDonorContributionTestCase.xml",
+                "SubmitStudentRecruiterTestCase.xml",
                 "TerminateEmployeeTestCase.xml"};
         for (int i = 0; i < asd.length; i++) {
             System.out.println("<exclude>" + asd[i] + "<exclude/>");
@@ -72,8 +72,8 @@ public class Workday {
 
     public static void main(String[] args) throws IOException {
             // PUT THE FILE NAME OF THE KEYS
-            Workday.getParametrization("Adoption.json");
-        //Workday.generateExclude();
+            Workday.getParametrization("IdentityManagement.json");
+//        Workday.generateExclude();
 
     }
 }
