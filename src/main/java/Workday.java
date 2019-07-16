7bbda35b41e76794c1e2cd3c9d324eb585bf20e7
Fiordalisi <te.fiordalisi@gmail.com>
2019-07-16 10:08:32 -0300

    Added regex for composite connector

--- src/main/java/Workday.java
+++ src/main/java/Workday.java
@@ -6,16 +6,24 @@ import java.io.IOException;
 import org.apache.commons.io.IOUtils;
 import java.io.FileInputStream;
 
+
 public class Workday {
 
-    public static void getParametrization(String jsonFile) throws IOException {
+    public static final String WORKDAY = "(\"displayName\":) (\\w*.*),";
+    public static final String COMPOSITE = "(\"id\":) (\\w*.*),\n";
 
-        final String regex = "(\"displayName\":) (\\w*.*),";
+
+
+    public static void getParametrization(String jsonFile, String connector) throws IOException {
+
+
         InputStream is = new FileInputStream("src/main/resources/" + jsonFile);
         String jsonTxt = IOUtils.toString(is, "UTF-8");
 
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Pattern pattern = Pattern.compile(connector, Pattern.MULTILINE);
         final Matcher matcher = pattern.matcher(jsonTxt);
+
+
 
         System.out.println("<munit:parameterizations>\n");
 
@@ -33,46 +41,10 @@ public class Workday {
         System.out.println("\t</munit:parameterizations>\n");
     }
 
-    public static void generateExclude(){
-        String asd[] = {"AddAcademicAppointmentTestCase.xml" ,
-                "CancelSupplierInvoiceTestCase.xml",
-                "CloseJobRequisitionTestCase.xml",
-                "EnterTimeOffTestCase.xml",
-                "GetCompensationPlansTestCase.xml",
-                "GetCustomListsTestCase.xml",
-                "GetDegreesTestCase.xml",
-                "GetEmployeeAwardsTestCase.xml",
-                "GetHeadcountPlanStructuresTestCase.xml",
-                "GetPaymentTypesTestCase.xml",
-                "GetPayrollCANYearEndsFormsTestCase.xml",
-                "GetPayrollPayeeNIsTestCase.xml",
-                "GetPayrollPayeePT1sTestCase.xml",
-                "GetPeriodSchedulesTestCase.xml",
-                "GetTextBlockCategoryTestCase.xml",
-                "GetWorkdayAccountSignOnsTestCase.xml",
-                "performance-management-flows.xml",
-                "performance-management-tests.xml",
-                "PutBenefitAnnualRateTestCase.xml",
-                "PutCompetencyTestCase.xml",
-                "PutFundTestCase.xml",
-                "PutIntegrationMessageTestCase.xml",
-                "PutStudentApplicationTestCase.xml",
-                "PutStudentCompetencyTestCase.xml",
-                "PutStudentEducationalInstitutionTestCase.xml",
-                "PutTimeClockEventsTestCase.xml",
-                "PutTranslatableTenantDataPublicTestCase.xml",
-                "SubmitDonorContributionIT.xml",
-                "SubmitDonorContributionTestCase.xml",
-                "SubmitStudentRecruiterTestCase.xml",
-                "TerminateEmployeeTestCase.xml"};
-        for (int i = 0; i < asd.length; i++) {
-            System.out.println("<exclude>" + asd[i] + "<exclude/>");
-        }
-    }
 
     public static void main(String[] args) throws IOException {
             // PUT THE FILE NAME OF THE KEYS
-            Workday.getParametrization("IdentityManagement.json");
+            Workday.getParametrization("IdentityManagement.json", WORKDAY);
 //        Workday.generateExclude();
 
     }
